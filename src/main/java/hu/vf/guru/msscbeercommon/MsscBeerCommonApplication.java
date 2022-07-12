ab00b648cba9de2fd2cd524fad1385272b08a2c3
vftake5 <vftake5@gmail.com>
2022-07-12 18:25:47 +0200

    Remove SpringBoot dependencies

--- src/main/java/hu/vf/guru/msscbeercommon/MsscBeerCommonApplication.java
+++ src/main/java/hu/vf/guru/msscbeercommon/MsscBeerCommonApplication.java
@@ -1,15 +1,11 @@
 package hu.vf.guru.msscbeercommon;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class MsscBeerCommonApplication
 {
 
 	public static void main(String[] args)
 	{
-		SpringApplication.run(MsscBeerCommonApplication.class, args);
+
 	}
 
 }
--- src/test/java/hu/vf/guru/msscbeercommon/MsscBeerCommonApplicationTests.java
+++ src/test/java/hu/vf/guru/msscbeercommon/MsscBeerCommonApplicationTests.java
@@ -1,13 +1,8 @@
 package hu.vf.guru.msscbeercommon;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class MsscBeerCommonApplicationTests
 {
 
-	@Test
 	void contextLoads()
 	{
 	}
